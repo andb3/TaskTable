02bdfe15c259ed85401b15b69817b74c578da9ca
andb3 <andb3003@yahoo.com>
2018-10-17 17:24:05 -0400

    putting off clay for a little bit

---
+++ src/js/config.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "TaskTable Configuration"
+  },
+  {
+    "type": "text",
+    "defaultValue": "Here is some introductory text."
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Amount of Rows"
+      },
+      {
+        "type": "color",
+        "messageKey": "BackgroundColor",
+        "defaultValue": "0x000000",
+        "label": "Background Color"
+      },
+      {
+        "type": "color",
+        "messageKey": "ForegroundColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Foreground Color"
+      }
+    ]
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Save Settings"
+  }
+];
