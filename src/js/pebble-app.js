0baaea51932fef63805517acfcc40638e143f1dc
andb3 <andb3003@yahoo.com>
2018-10-24 17:34:17 -0400

    custom config file (github pages)

--- src/js/pebble-app.js
+++ src/js/pebble-app.js
@@ -1,3 +1,10 @@
+// Import the Clay package
+var Clay = require('pebble-clay');
+// Load our Clay configuration file
+var clayConfig = require('./config');
+// Initialize Clay
+var clay = new Clay(clayConfig); 
+
 var table_size = 8;
 
 // Initializing a class definition
