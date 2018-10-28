693c0f0c272c2c06285fea3600a850df5aa8e3ec
andb3 <andb3003@yahoo.com>
2018-10-27 22:19:54 -0400

    remove ranges

--- docs/js/timeranges.js
+++ docs/js/timeranges.js
@@ -1,6 +1,6 @@
 var rangeCache = document.getElementsByClassName("range");
 
-var rangehtml = '<p id="rangetext">Range <span class="rangenumber" id="rangenumber"></span></p><input type="time" name="starttime"  id="starttime" class="starttime"><p id="rangetext">-</p><input type="time" name="endtime" id="endtime" class="endtime"><div class="range_row" id="range_row"><select id="rangedropdown"></select></div>';
+var rangehtml = '<p id="rangetext">Range <span class="rangenumber" id="rangenumber"></span></p><input type="time" name="starttime"  id="starttime" class="starttime"><p id="rangetext">-</p><input type="time" name="endtime" id="endtime" class="endtime"><div class="range_row" id="range_row"><select id="rangedropdown"></select></div><button class="remove_range_button"><i class="material-icons">clear</i></button>';
 
 function resetRanges(){
 
@@ -24,7 +24,34 @@ function newRange(){
 
   rangeholder.appendChild(appDiv);
 
+  var removebutton = appDiv.getElementsByClassName("remove_range_button")[0];
+
+  var elementToRemove = rangeholder.childElementCount - 1;
+
+  removebutton.addEventListener("click", function(e) {
+    //e.stopPropagation();
+    removeRange(elementToRemove);
+  });
+
   setRangeDropdowns();
+}
+
+function removeRange(toRemove){
+
+
+  var rangeholder = document.getElementById("range_holder");
+
+  console.log("removing element " + toRemove + " of " + rangeholder.childElementCount);
+
+  rangeholder.removeChild(rangeholder.children[toRemove]);
+  //rangeholder.removeChild(rangeholder.childNodes[toRemove]);
+
+
+  var ranges = rangeholder.getElementsByClassName("rangenumber");
+
+  for (var i = toRemove; i<ranges.length; i++){
+    ranges[i].innerHTML = i+1;
+  }
 }
 
 function checkTimes(){
